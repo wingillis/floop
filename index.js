a9d3b19daa5503417520bbe27185c91efdebef17
wingillis <win.gillis@gmail.com>
2019-02-18 19:23:43 -0500

    added comments

--- index.js
+++ index.js
@@ -2,25 +2,35 @@ const fs = require('fs');
 const pdf = require('pdf-parse');
 const request = require('superagent');
 
+// link to the crossref api
 const crossref = 'https://api.crossref.org/works/';
 
+// I have a file on my desktop for testing
 let file = '/Users/wgillis/Desktop/1-s2.0-S0959438808000767-main.pdf'
+// contains the pdf file in memory now
 let databuffer = fs.readFileSync(file);
 
-console.log(process.argv)
+//console.log(process.argv)
 
+// parse the pdf then ...
 pdf(databuffer).then(function(data) {
+	// print the information about the pdf, i.e. title, etc.
 	console.log(data.info);
 	let title = data.info.Title;
 	if (title.includes('doi')) {
 		let re = /doi:/;
+		// remove doi from the title name
 		doi = title.replace(re, '');
 		console.log('Searching for title');
+		// use crossref api to get actual pdf title name
 		request.get(crossref + doi).end((err, res) => {
 			let msg = res.body.message;
+			// the paper's title
 			let worktitle = msg.title[0];
 			console.log(worktitle);
+			// this is everything that crossref sends back
 			console.log(msg);
+			// this is the year the paper was published
 			console.log(msg.issued['date-parts'][0][0]);
 		});
 	}
