53796d27cee5b538f9642ff7d72c16e46466dd4a
wingillis <win.gillis@gmail.com>
2019-02-25 21:29:26 -0500

    adding in all programmatic features for doi detection

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const getDOIandTitle = require('./lib/pdfs.js')
-const assert = require('assert');
-const request = require('superagent');
+const io = require('./lib/io.js')
+const assert = require('assert')
+const request = require('superagent')
+const join = require('path').join
+
+// load in the config file
 
 // link to the crossref api
 const crossref = 'https://api.crossref.org/works/';
 
-
-async function getDataFromCrossref(doi) {
+function getDataFromCrossref(doi) {
   return new Promise((resolve, reject) => {
+      // request.get(crossref + doi).end((err, res) => {
       request.get(crossref + doi).end((err, res) => {
       let msg = res.body.message
       // the paper's title
@@ -18,22 +22,71 @@ async function getDataFromCrossref(doi) {
         title: worktitle,
         publisher: publisher,
         year: msg.issued['date-parts'][0][0],
-        authors: msg.author
+        authors: msg.author,
+        doi: doi
       })
     })
   })
 }
 
+async function getDOIFromPII(pii) {
+  pii = pii.replace(/[()-]/g, '')
+  return new Promise((resolve, reject) => {
+    request.get('https://api.crossref.org/works?query=' + pii).end((err, res) => {
+      let msg = res.body.message
+      if (msg.items.length == 1) {
+        resolve(msg.items[0].DOI)
+      } else {
+        console.log(msg.items)
+        reject('Too many parameters and I do not know how to deal with it yet')
+      }
+    })
+  })
+}
+
+async function getCrossref(doi) {
+  return await getDataFromCrossref(doi)
+}
+
+async function watchLoop (config) {
+  let files = io.scanFolder(config.watch)
+  for (let f in files) {
+    let data = await getDOIandTitle(files[f])
+
+    if (data.title === 'pii') {
+      data.doi = await getDOIFromPII(data.doi)
+      data.title = null
+    }
+    if (data.doi != null) {
+      let crossref_data = getDataFromCrossref(data.doi)
+      // eventually, search the mini json db for entries matching title, doi or 
+      // io.moveItem(files[f], join(config.outputDirectory, 'untagged'))
+      // console.log(crossref_data)
+    } else if (data.doi == null && data.title == null) {
+      // move these files to an 'unprocessed' folder
+      io.moveItem(files[f], join(config.outputDirectory, 'unprocessed'))
+    }
+  }
+  // files.every((f) => {
+  //   console.log(data)
+  //   let crossref_data = getCrossref(data.doi)
+  //   // eventually move files into the output directory
+  // })
+  // _.map(files, (f) => {
+  //   io.moveItem(f, config.outputDirectory)
+  // })
+}
+
 
 async function main() {
   // put in everything that I'll use to parse stuff here
-  assert(process.argv.length > 2, 'Please specify an input file')
-  let file = process.argv[2]
-  console.log(file)
-  let d = await getDOIandTitle(file)
-  let thing = await getDataFromCrossref(d.doi)
-  console.log(`title: "${d.title}"`)
-  console.log(thing)
+  const config = require('./config.json')
+  config.scanEvery = config.scanEvery * 1000 * 60
+  watchLoop(config)
+
+  // this is the watching interval
+  // let intervalID = setInterval(watchLoop, config.scanEvery, config)
+
 }
 
 main()
